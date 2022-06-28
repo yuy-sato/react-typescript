427f0a971d3782ab9e9ff740cb3b31fb79bb829f
yuya.sato <yuya.sato@livesense.co.jp>
2022-06-28 17:59:03 +0900

    高階関数にtypescriptの型をつける

--- src/lib/withLog.tsx
+++ src/lib/withLog.tsx
@@ -3,8 +3,14 @@
 // 引数に文字列を受け取って、コンポーネントを引数に受け取って文字列をconsole.logで出力するコンポーネントを返す
 import React from 'react';
 
-export const withLog = (message: string) => {
-  return (Component: any) => {
+type ComponentProps = {
+  message: string;
+};
+
+type Fn = (message: string) => (Component: React.FC<ComponentProps>) => React.FC;
+
+export const withLog: Fn = (message: string) => {
+  return (Component) => {
     const WrapperComponent = () => {
       return (
         <>
